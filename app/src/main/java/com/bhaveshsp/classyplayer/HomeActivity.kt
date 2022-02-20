017e2f4383779ed19be976e53bc23497755684d3
Bhavesh <Bhaveshsp8624@gmail.com>
2022-02-20 13:38:09 -0500

    Changed Bottom View Color Changed Navigation View Color Fixed the Drawer Not Opening Issue Added Navigation to Both Drawer and Bottom Navigation

--- app/src/main/java/com/bhaveshsp/classyplayer/HomeActivity.kt
+++ app/src/main/java/com/bhaveshsp/classyplayer/HomeActivity.kt
@@ -5,15 +5,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.res.ResourcesCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 
@@ -21,36 +23,44 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var drawerToggle: ActionBarDrawerToggle
     private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         val toolbar : Toolbar = findViewById(R.id.toolbar)
+//        toolbar.setNavigationIcon(R.drawable.nav_drawer_icon)
         setSupportActionBar(toolbar)
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
-        drawerToggle = ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close)
-        drawerToggle.isDrawerIndicatorEnabled = false
-        val drawerDrawable : Drawable = ResourcesCompat.getDrawable(resources,R.drawable.nav_drawer_icon,this.theme)!!
-        drawerToggle.setHomeAsUpIndicator(drawerDrawable)
+        drawerLayout = findViewById(R.id.drawerLayout)
 
+//        drawerToggle.setToolbarNavigationClickListener {
+//            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+//
+//        }
         appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.navVideos,
-            R.id.navMusic
+            R.id.nav_videos,
+            R.id.nav_music
         ).setOpenableLayout(drawerLayout).build()
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
         val navigationView = findViewById<NavigationView>(R.id.navigationView)
 
         val navController = Navigation.findNavController(this,R.id.nav_host_fragment_main)
         NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration)
         NavigationUI.setupWithNavController(navigationView,navController)
+        bottomNavigationView.setupWithNavController(navController)
+
+
+        drawerToggle = ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close)
+        drawerToggle.isDrawerIndicatorEnabled = false
+        val drawerDrawable : Drawable = ResourcesCompat.getDrawable(resources,R.drawable.nav_drawer_icon,this.theme)!!
+        drawerToggle.setHomeAsUpIndicator(drawerDrawable)
 
         drawerLayout.addDrawerListener(drawerToggle)
         drawerToggle.syncState()
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setDisplayShowTitleEnabled(false)
-
-
-
 
     }
 
@@ -59,18 +69,13 @@ class HomeActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (drawerToggle.onOptionsItemSelected(item)){
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = Navigation.findNavController(this,R.id.nav_host_fragment_main)
         return NavigationUI.navigateUp(navController,appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+
 }
 
 
